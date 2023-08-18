a559185380cba897314f47bb064550081db51fdb
mainangaruiya <003paulmaina@gmail.com>
2023-08-18 16:52:24 +0300

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,7 @@
-#!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
-"""
 from flask import Flask
-
+''''start web flask and display hello world'''
 app = Flask(__name__)
 
-
-@app.route("/", strict_slashes=False)
+@app.route('/airbnb-onepage/', methods=['GET'])
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
